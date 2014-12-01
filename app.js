41462c34d3540b98c84bd494433e6f4ad44de3cd
benno001 <bjf.dehaan@gmail.com>
2014-12-01 16:02:29 +0100

    Models

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongoose');
+
+var uri = "mongodb://localhost/project";
+
+mongo.connect(uri);
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
