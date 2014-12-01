6b5acdd8a4fc2d3f77703fc54e6230407b8a3eeb
benno001 <bjf.dehaan@gmail.com>
2014-12-01 18:02:17 +0100

    Angular is working, see index.ejs in view folder

--- models/Categories.js
+++ models/Categories.js
@@ -2,8 +2,8 @@
  * Database schema for posts
  */
 
-
 var mongoose = require('mongoose');
+
 var CategorySchema = new mongoose.Schema({
 	title: String,
 	post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
